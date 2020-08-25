05dcbe752a5badc6336270e37c43ab8b94268573
agdholo <agdholo@gmail.com>
2020-08-26 00:19:24 +0800

    3.3 静态页面

--- src/App.vue
+++ src/App.vue
@@ -34,20 +34,23 @@
     </v-app-bar>
 
     <v-main>
-      <HelloWorld />
+      <v-container>
+        <h1>登录表单</h1>
+        <v-text-field label="邮件地址" type="email"></v-text-field>
+        <v-text-field label="密码" type="password"></v-text-field>
+        <v-btn rounded>
+          登录
+        </v-btn>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
-
 export default {
   name: "App",
 
-  components: {
-    HelloWorld
-  },
+  components: {},
 
   data: () => ({
     //
