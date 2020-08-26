6b300a94bf6684e46cf7f6bd081b08e891cc5cf4
agdholo <agdholo@gmail.com>
2020-08-27 01:06:28 +0800

    6.5前端页面

--- src/main.js
+++ src/main.js
@@ -4,12 +4,14 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import axios from "./plugins/axios";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  axios,
   vuetify,
   render: h => h(App)
 }).$mount("#app");
--- src/views/Sign.vue
+++ src/views/Sign.vue
@@ -1,8 +1,58 @@
 <template>
   <div>
     <h1>注册表单</h1>
-    <v-text-field label="邮件地址" type="email"></v-text-field>
-    <v-text-field label="密码" type="password"></v-text-field>
-    <v-btn rounded>注册</v-btn>
+
+    <v-alert
+      dense
+      outlined
+      :type="success ? 'success' : 'error'"
+      v-if="success || error"
+    >
+      <div v-if="error">
+        <span v-for="(errorItem, i) in error" :key="i">
+          {{ errorItem }}
+          <br />
+        </span>
+      </div>
+
+      <div v-if="success">注册成功，欢迎你： {{ success.email }}</div>
+    </v-alert>
+
+    <v-text-field label="邮件地址" type="email" v-model="email"></v-text-field>
+    <v-text-field
+      label="密码"
+      type="password"
+      v-model="password"
+    ></v-text-field>
+    <v-btn rounded @click="sign">注册</v-btn>
   </div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    email: "",
+    password: "",
+    error: "",
+    success: ""
+  }),
+  methods: {
+    sign() {
+      const api = "http://127.0.0.1:8000/sign";
+      this.error = "";
+      this.success = "";
+      this.axios
+        .post(api, {
+          email: this.email,
+          password: this.password
+        })
+        .then(response => {
+          this.success = response.data;
+        })
+        .catch(error => {
+          this.error = error.response.data;
+        });
+    }
+  }
+};
+</script>
