994c06e2487f893f321000d2973947fe9552d2ac
agdholo <agdholo@gmail.com>
2020-09-10 11:05:45 +0800

    chore: change api domain

--- model/api.js
+++ model/api.js
@@ -5,7 +5,7 @@ import axios from "./http";
 import store from "../src/store/index";
 import router from "../src/router/index";
 
-const domain = "http://127.0.0.1:8000";
+const domain = "http://tp6-p01.api.heroui.net:1111/";
 
 const api = {
   tweetFetch(params) {
