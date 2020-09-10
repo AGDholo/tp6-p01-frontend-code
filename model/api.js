e76198062f45cf7bd101b68120cb24cd2d9c71e9
agdholo <agdholo@gmail.com>
2020-09-10 11:33:13 +0800

    update

--- model/api.js
+++ model/api.js
@@ -5,7 +5,7 @@ import axios from "./http";
 import store from "../src/store/index";
 import router from "../src/router/index";
 
-const domain = "/api";
+const domain = "http://127.0.0.1:8000";
 
 const api = {
   tweetFetch(params) {
